552b0062be81c575cc1c3ba648299ba877c9e935
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-26 16:05:17 +0700

    menus done

--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -3,6 +3,8 @@ import Fruit1 from "../../assets/fruits/apple.png";
 import Fruit2 from "../../assets/fruits/orange.png";
 import Fruit3 from "../../assets/fruits/avocado.png";
 import Fruit4 from "../../assets/fruits/cherry.png";
+import { motion, AnimatePresence } from "framer-motion";
+import { FadeLeft } from "../../utility/animation";
 
 const MenuData = [
   {
@@ -11,6 +13,7 @@ const MenuData = [
     link: "/",
     price: "$1.99",
     img: Fruit1,
+    delay: 0.3,
   },
   {
     id: 2,
@@ -18,6 +21,7 @@ const MenuData = [
     link: "/",
     price: "$2.99",
     img: Fruit2,
+    delay: 0.6,
   },
   {
     id: 3,
@@ -25,6 +29,7 @@ const MenuData = [
     link: "/",
     price: "$4.99",
     img: Fruit3,
+    delay: 0.9,
   },
   {
     id: 4,
@@ -32,6 +37,7 @@ const MenuData = [
     link: "/",
     price: "$5.99",
     img: Fruit4,
+    delay: 1.2,
   },
 ];
 
@@ -39,18 +45,36 @@ const Menus = () => {
   return (
     <section>
       <div className="container pt-12 pb-20 border-2 border-red-600">
-        <h1 className="text-2xl font-bold text-left pb-10 uppercase">
+        <motion.h1
+          initial={{ opacity: 0, x: -200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.3 }}
+          className="text-2xl font-bold text-left pb-10 uppercase"
+        >
           Our Menu
-        </h1>
+        </motion.h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
           {MenuData.map((menu) => (
-            <div key={menu.id} className="bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row justify-around items-center gap-3">
-              <img src={menu.img} alt={menu.title} className="w-[60px] mb-4 scale-110 transform-translate-y-6"/>
+            <motion.div
+              variants={FadeLeft(menu.delay)}
+              initial="hidden"
+              whileInView={"visible"}
+              whileHover={{ scale: 1.1 }}
+              key={menu.id}
+              className="bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row justify-around items-center gap-3"
+            >
+              <img
+                src={menu.img}
+                alt={menu.title}
+                className="w-[60px] mb-4 scale-110 transform-translate-y-6"
+              />
               <div>
                 <h1 className="text-lg  font-semibold">{menu.title}</h1>
-                <p className="text-lg font-semibold text-secondary">{menu.price}</p>
+                <p className="text-lg font-semibold text-secondary">
+                  {menu.price}
+                </p>
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
