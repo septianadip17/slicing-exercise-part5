cfa2a39ac3e2ee22aadbb58f1cbb53ef95fc8070
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-26 15:14:30 +0700

    menus added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Menus from "./components/Menus/Menus";
 function App() {
   return (
     <>
       <main className="overflow-x-hidden">
         <Navbar />
         <Hero />
+        <Menus />
       </main>
     </>
   );
