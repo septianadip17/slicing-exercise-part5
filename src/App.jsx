39fa0ca92e60375e71130a9b5d854cd063aef168
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-26 12:55:34 +0700

    hero added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 function App() {
   return (
     <>
       <main className="overflow-x-hidden">
         <Navbar />
+        <Hero />
       </main>
     </>
   );
