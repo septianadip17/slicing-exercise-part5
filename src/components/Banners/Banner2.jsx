a363dced76a96567502f681c149b6e183ed77bf2
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-27 01:45:56 +0700

    banner 2 image showed

---
+++ src/components/Banners/Banner2.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import BannerPng from "../../assets/fruit-plate2.png";
+import { motion } from "framer-motion";
+import { FadeUp } from "../../utility/animation";
+
+const Banner2 = () => {
+  return (
+    <section className="">
+      <div className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14 md:py-24">
+        {/* Brand Info */}
+        <div className="flex flex-col justify-center">
+          <div className="text-center md:text-left space-y-4 lg:max-w-[400px]">
+            <motion.h1
+              variants={FadeUp(0.5)}
+              initial="hidden"
+              whileInView="visible"
+              className="text-3xl lg:text:6xl font-bold uppercase"
+            >
+              Brand Info
+            </motion.h1>
+            <motion.p
+              variants={FadeUp(0.7)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+              assumenda officia accusantium non, repellendus iure incidunt
+              voluptatem dolor nihil dolores magnam velit quos, impedit aliquid
+              repudiandae architecto, obcaecati illo aspernatur.
+            </motion.p>
+            <motion.p
+              variants={FadeUp(0.9)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              assumenda illo, voluptatem iure enim blanditiis!
+            </motion.p>
+            <motion.div
+              variants={FadeUp(1.1)}
+              initial="hidden"
+              animate="visible"
+              className="flex justify-center md:justify-start"
+            >
+              <button className="primary-btn">Learn More</button>
+            </motion.div>
+          </div>
+        </div>
+        {/* Banner Image */}
+        <div className="flex justify-center items-center">
+          <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
+            viewport={{ once: true }}
+            src={BannerPng}
+            alt="fruits splash"
+            className="w-[300px] md:max-w-[400px] h-full object-cover"
+          />
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Banner2;
