6a60723992165c4498d437c12849b8f7e47211d5
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-26 10:51:07 +0700

    navbar logo and menu section finished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,62 @@
 import React from "react";
 import { FaLeaf } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
 
+const NavbarMenu = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Products",
+    link: "/products",
+  },
+  {
+    id: 3,
+    title: "About",
+    link: "/about",
+  },
+  {
+    id: 4,
+    title: "Shop",
+    link: "/shop",
+  },
+  {
+    id: 5,
+    title: "Contacts",
+    link: "/contacts",
+  },
+];
 const Navbar = () => {
   return (
     <nav>
-      <div className="container">
+      <div className="container flex justify-between items-center py-4 md:pt-4">
         {/* Logo section */}
-        <div>
-          <p>Fruit</p>
-          <p>Store</p>
-          <FaLeaf />
+        <div className="text-2xl flex items-center gap-2 font-bold uppercase">
+          <p className="text-primary">Fruit</p>
+          <p className="text-secondary">Store</p>
+          <FaLeaf className="text-green-500" />
         </div>
         {/* Menu section */}
+        <div className="hidden md:block">
+          <ul className="flex items-center gap-6 text-gray-600">
+            {NavbarMenu.map((menu) => (
+              <li key={menu.id} className="text-xl">
+                <a
+                  href={menu.link}
+                  className="inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold"
+                >
+                  {menu.title}
+                </a>
+              </li>
+            ))}
+            <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+              <MdOutlineShoppingCart />
+            </button>
+          </ul>
+        </div>
       </div>
     </nav>
   );
