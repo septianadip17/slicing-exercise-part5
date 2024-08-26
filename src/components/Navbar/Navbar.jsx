1e4d285f8354d26df14ec6689f3c09cf523a5225
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-26 12:54:30 +0700

    humberger finished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,42 +31,47 @@ const NavbarMenu = [
   },
 ];
 const Navbar = () => {
+  const [open, setOpen] = React.useState(false);
   return (
-    <nav>
-      <div className="container flex justify-between items-center py-4 md:pt-4">
-        {/* Logo section */}
-        <div className="text-2xl flex items-center gap-2 font-bold uppercase">
-          <p className="text-primary">Fruit</p>
-          <p className="text-secondary">Store</p>
-          <FaLeaf className="text-green-500" />
-        </div>
+    <>
+      <nav>
+        <div className="container flex justify-between items-center py-4 md:pt-4">
+          {/* Logo section */}
+          <div className="text-2xl flex items-center gap-2 font-bold uppercase">
+            <p className="text-primary">Fruit</p>
+            <p className="text-secondary">Store</p>
+            <FaLeaf className="text-green-500" />
+          </div>
 
-        {/* Menu section */}
-        <div className="hidden md:block">
-          <ul className="flex items-center gap-6 text-gray-600">
-            {NavbarMenu.map((menu) => (
-              <li key={menu.id} className="text-xl">
-                <a
-                  href={menu.link}
-                  className="inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold"
-                >
-                  {menu.title}
-                </a>
-              </li>
-            ))}
-            <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
-              <MdOutlineShoppingCart />
-            </button>
-          </ul>
-        </div>
+          {/* Menu section */}
+          <div className="hidden md:block">
+            <ul className="flex items-center gap-6 text-gray-600">
+              {NavbarMenu.map((menu) => (
+                <li key={menu.id} className="text-xl">
+                  <a
+                    href={menu.link}
+                    className="inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold"
+                  >
+                    {menu.title}
+                  </a>
+                </li>
+              ))}
+              <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+                <MdOutlineShoppingCart />
+              </button>
+            </ul>
+          </div>
 
-        {/* Mobile Hamburger Menu section */}
-        <div className="md:hidden">
-          <MdMenu className="text-4xl" />
+          {/* Mobile Hamburger Menu section */}
+          <div className="md:hidden" onClick={() => setOpen(!open)}>
+            <MdMenu className="text-4xl" />
+          </div>
         </div>
-      </div>
-      <ResponsiveMenu />
-    </nav>
+      </nav>
+
+      {/* Mobile Menu Section */}
+      <ResponsiveMenu open={open}/>
+    </>
   );
 };
 
--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-const ResponsiveMenu = () => {
+const ResponsiveMenu = ({ open }) => {
   return (
-<div>ResponsiveMenu</div>
-  )
-}
+    <AnimatePresence mode="wait">
+      {open && (
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -100 }}
+          transition={{ duration: 0.3 }}
+          className="absolute top-20 left-0 w-full h-screen z-2"
+        >
+          <div className="text-xl font-semibold uppercase bg-primary text-white py-10 m-6 rounded-3xl">
+            <ul className="flex flex-col items-center gap-10">
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
 
-export default ResponsiveMenu 
+export default ResponsiveMenu;
