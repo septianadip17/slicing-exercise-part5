72215a432aeef7ee6c03baeda8bc366bc9dad6a1
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-26 10:32:42 +0700

    react icons installed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { FaLeaf } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav>Navbar</nav>
-  )
-}
+    <nav>
+      <div className="container">
+        {/* Logo section */}
+        <div>
+          <p>Fruit</p>
+          <p>Store</p>
+          <FaLeaf />
+        </div>
+        {/* Menu section */}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
