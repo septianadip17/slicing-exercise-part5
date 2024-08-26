61d039581613da8d7bffca890072ab4e6a20da29
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-26 10:58:27 +0700

    responsive menu showed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import ResponsiveMenu from "./components/Navbar/ResponsiveMenu";
 function App() {
   return (
     <>
       <main className="overflow-x-hidden">
         <Navbar />
+        <ResponsiveMenu />
       </main>
     </>
   );
