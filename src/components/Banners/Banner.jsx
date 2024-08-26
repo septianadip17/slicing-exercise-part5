956be30a064274ea5938818d5f5a05f05db283e2
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-27 01:39:19 +0700

    banner 1 finished

--- src/components/Banners/Banner.jsx
+++ src/components/Banners/Banner.jsx
@@ -5,11 +5,15 @@ import { FadeUp } from "../../utility/animation";
 
 const Banner = () => {
   return (
-    <section>
-      <div className="container">
+    <section className="bg-secondary/10">
+      <div className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14">
         {/* Banner Image */}
-        <div>
-          <img
+        <div className="flex justify-center items-center">
+          <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
+            viewport={{ once: true }}
             src={BannerPng}
             alt="fruits splash"
             className="w-[300px] md:max-w-[400px] h-full object-cover"
@@ -17,21 +21,38 @@ const Banner = () => {
         </div>
 
         {/* Brand Info */}
-        <div>
-          <div>
-            <h1>Brand Info</h1>
-            <p>
+        <div className="flex flex-col justify-center">
+          <div className="text-center md:text-left space-y-4 lg:max-w-[400px]">
+            <motion.h1
+              variants={FadeUp(0.5)}
+              initial="hidden"
+              whileInView="visible"
+              className="text-3xl lg:text:6xl font-bold uppercase"
+            >
+              Brand Info
+            </motion.h1>
+            <motion.p
+              variants={FadeUp(0.7)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
               assumenda officia accusantium non, repellendus iure incidunt
               voluptatem dolor nihil dolores magnam velit quos, impedit aliquid
               repudiandae architecto, obcaecati illo aspernatur.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p
+              variants={FadeUp(0.9)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
               assumenda illo, voluptatem iure enim blanditiis!
-            </p>
+            </motion.p>
             <motion.div
-              variants={FadeUp(1.5)}
+              variants={FadeUp(1.1)}
               initial="hidden"
               animate="visible"
               className="flex justify-center md:justify-start"
