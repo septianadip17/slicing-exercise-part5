5309b084cf030115eea4e7b89ffd12a683c32d3d
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-27 08:05:00 +0700

    mt12 added

--- src/components/Footer.jsx/Footer.jsx
+++ src/components/Footer.jsx/Footer.jsx
@@ -6,10 +6,11 @@ const Footer = () => {
   return (
     <footer className="bg-primary/10 py-12 mt-12">
       <motion.div
-      initial={{opacity: 0}}
-      whileInView={{opacity: 1}}
-      transition={{duration: 1, delay: 0.2}}
-      className="container flex justify-between items-center">
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="container flex justify-between items-center"
+      >
         {/* Logo section */}
         <div className="text-2xl flex items-center gap-2 font-bold uppercase">
           <p className="text-primary">Fruit</p>
