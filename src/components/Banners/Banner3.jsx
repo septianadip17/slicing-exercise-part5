3fad5c19a1175097be48902fed312102c559fbc9
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-27 08:06:00 +0700

    container mb-12 added in section

--- src/components/Banners/Banner3.jsx
+++ src/components/Banners/Banner3.jsx
@@ -11,7 +11,7 @@ const bgStyle = {
 };
 const Banner3 = () => {
   return (
-    <section className="">
+    <section className="container mb-12">
       <div
         style={bgStyle}
         className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14"
