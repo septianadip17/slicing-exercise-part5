9ff82b7af935f5bd4122cc5f762b0e2a6ac9d282
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-27 07:51:10 +0700

    finished banner 3

--- src/components/Banners/Banner3.jsx
+++ src/components/Banners/Banner3.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import BannerPng from "../../assets/banner-bg.jpg";
+import { motion } from "framer-motion";
+import { FadeLeft } from "../../utility/animation";
 
+const bgStyle = {
+  backgroundImage: `url(${BannerPng})`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
 const Banner3 = () => {
   return (
-    <div>Banner3</div>
-  )
-}
+    <section className="">
+      <div
+        style={bgStyle}
+        className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14"
+      >
+        {/* blank div */}
+        <div></div>
 
-export default Banner3
+        {/* Brand Info */}
+        <div className="flex flex-col justify-center">
+          <div className="text-center md:text-left space-y-4 lg:max-w-[400px]">
+            <motion.h1
+              variants={FadeLeft(0.5)}
+              initial="hidden"
+              whileInView="visible"
+              className="text-3xl lg:text:6xl font-bold uppercase"
+            >
+              Brand Info
+            </motion.h1>
+            <motion.p
+              variants={FadeLeft(0.7)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+              assumenda officia accusantium non, repellendus iure incidunt
+              voluptatem dolor nihil dolores magnam velit quos, impedit aliquid
+              repudiandae architecto, obcaecati illo aspernatur.
+            </motion.p>
+
+            <motion.div
+              variants={FadeLeft(0.9)}
+              initial="hidden"
+              animate="visible"
+              className="flex justify-center md:justify-start"
+            >
+              <button className="primary-btn">Learn More</button>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner3;
