f89caaf336266bd850768f74b7e0beadd5a72da9
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-27 08:02:21 +0700

    footer done

--- src/components/Footer.jsx/Footer.jsx
+++ src/components/Footer.jsx/Footer.jsx
@@ -1,8 +1,30 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { FaFacebookF, FaTwitter, FaInstagram, FaLeaf } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary/10 py-12">
+      <motion.div
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
+      transition={{duration: 1, delay: 0.2}}
+      className="container flex justify-between items-center">
+        {/* Logo section */}
+        <div className="text-2xl flex items-center gap-2 font-bold uppercase">
+          <p className="text-primary">Fruit</p>
+          <p className="text-secondary">Store</p>
+          <FaLeaf className="text-green-500" />
+        </div>
+        {/* Social icons section */}
+        <div className="text-3xl flex items-center gap-4 mt-6 text-gray-700">
+          <FaInstagram />
+          <FaTwitter />
+          <FaFacebookF />
+        </div>
+      </motion.div>
+    </footer>
+  );
 };
 
 export default Footer;
